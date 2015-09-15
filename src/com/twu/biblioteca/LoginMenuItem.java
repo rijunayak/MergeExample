1199c63e035ff05d64b57b605f108f6b13a3f3e4
rijun <rijun@INrijun.local>
2015-09-15 19:41:13 +0530

    Login menu item should return success message on a successfull login

--- src/com/twu/biblioteca/LoginMenuItem.java
+++ src/com/twu/biblioteca/LoginMenuItem.java
@@ -1,18 +1,37 @@
 //A Login  menu item takes credentials from the user and logs in
 package com.twu.biblioteca;
 
-public class LoginMenuItem {
+public class LoginMenuItem implements MenuItem {
 
     private ConsoleInput consoleInput;
     private Session session;
 
-    public LoginMenuItem(Session session) {
-        this.consoleInput = new ConsoleInput();
+    public LoginMenuItem(Session session, ConsoleInput consoleInput) {
+        this.consoleInput = consoleInput;
         this.session = session;
     }
 
     @Override
     public String toString() {
         return "Login";
+    }
+
+    @Override
+    public String doOperation() {
+        String userIDToLogin = "";
+        String password = "";
+        Authenticator authenticator = new Authenticator();
+        System.out.print("\nLogin : ");
+        userIDToLogin = consoleInput.getInput();
+        System.out.print("\nPassword : ");
+        password = consoleInput.getInput();
+        User userToBePutInSession = authenticator.authenticate(userIDToLogin, password);
+        if(userToBePutInSession != null) {
+            session = new Session(userToBePutInSession);
+            return "Logged In!";
+        } else {
+            session = new Session(userToBePutInSession);
+            return "Invalid Credentials.";
+        }
     }
 }
--- test/com/twu/biblioteca/LoginMenuItemTest.java
+++ test/com/twu/biblioteca/LoginMenuItemTest.java
@@ -4,14 +4,27 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class LoginMenuItemTest {
 
     @Test
-    public void LoginMenuItemShouldReturnRightEntryInItsMenu() {
+    public void shouldReturnRightEntryInItsMenu() {
         Session session = mock(Session.class);
-        LoginMenuItem loginMenuItem = new LoginMenuItem(session);
+        ConsoleInput consoleInput = mock(ConsoleInput.class);
+        LoginMenuItem loginMenuItem = new LoginMenuItem(session, consoleInput);
 
         assertEquals("Login", loginMenuItem.toString());
+    }
+
+    @Test
+    public void shouldRightlyReturnSuccessMessageOnLoginOperation() {
+        Session session = mock(Session.class);
+        ConsoleInput consoleInput = mock(ConsoleInput.class);
+        LoginMenuItem loginMenuItem = new LoginMenuItem(session, consoleInput);
+
+        when(consoleInput.getInput()).thenReturn("user1", "password1");
+
+        assertEquals("Logged In!", loginMenuItem.doOperation());
     }
 }
