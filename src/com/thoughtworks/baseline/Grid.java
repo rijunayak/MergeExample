7f0689b965b50afbe66fe81f2abfe222e4d808ad
rijun <rijun@INrijun.local>
2015-09-19 12:52:45 +0530

    An alive state should be returned for an alive state if only its left cell and diagnol north right cell is alive

--- src/com/thoughtworks/baseline/Grid.java
+++ src/com/thoughtworks/baseline/Grid.java
@@ -31,6 +31,8 @@ public class Grid {
             noOfAliveNeighbours++;
         if(gridOfCells.get(i - 2).get(j - 1).stateOfCell())
             noOfAliveNeighbours++;
+        if(gridOfCells.get(i - 2).get(j).stateOfCell())
+            noOfAliveNeighbours++;
         if(noOfAliveNeighbours == 2) {
             return true;
         }
